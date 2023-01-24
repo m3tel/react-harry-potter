549f9be75b86ca6172704f8c70bf5ca8d1f6c82c
m3tel <m3tel161@mail.ru>
2023-01-24 15:03:02 +0300

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { Header } from "../components/Header/Header";
+import { Main } from "../components/Main/Main";
+import { data } from "../components/Cards/hp";
+import { useState } from "react";
+
+export function App() {
+  const [hero, setHero] = useState(data);
+  const [house, setHouse] = useState('');
+                     
+  function searchHero(event) {
+    const hero = data
+      .filter((el) =>
+        el.name.toLowerCase().includes(event.target.value.trim().toLowerCase()))
+  
+    setHero(hero);   
+  }
+
+  return (
+    <>
+      <Header />
+      <Main
+        hero={hero}
+        house={house}
+        searchHero={searchHero}
+        setHouse={setHouse} />
+    </>
+  );
+}
+
+
